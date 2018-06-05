7b2a9d2578b5a1bcbd866472d87810116cb811ef
mreficent <miquel.raich@eficent.com>
2018-06-05 13:47:32 +0200

    [ADD] gdpr_partner_report

---
+++ data_protection_base/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'Data Protection Base',
+    'version': '9.0.1.0.0',
+    'category': '??',
+    'summary': 'Provides security and menus for data protection modules.',
+    'author': "Eficent, "
+              "Odoo Community Association (OCA)",
+    'website': 'http://www.github.com/Eficent/gdpr,',
+    'license': 'AGPL-3',
+    'data': [
+        'security/data_protection.xml',
+        'views/data_protection_menu_view.xml',
+    ],
+    'installable': True,
+}

8f095a42a4cb56b81ce16eaaff789c68b333aaa6
Daniel Reis <dreis.pt@hotmail.com>
2018-06-05 23:03:37 +0100

    Structure the privacy_base app module
    Renamed module to use the privacy_ convention.
    Removed report module (to be proposed in own PR).
    Changed README to the autogenerated new style.
    
    Fix website URL
    
    rename

---
+++ privacy/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'Data Privacy and Protection',
+    'version': '9.0.1.0.0',
+    'category': 'Data Protection',
+    'summary': 'Provides data privacy and protection features '
+               'to comply to regulations, such as GDPR.',
+    'author': "Eficent, "
+              "Odoo Community Association (OCA)",
+    'website': 'http://www.github.com/OCA/data-protection',
+    'license': 'AGPL-3',
+    'data': [
+        'security/data_protection.xml',
+        'views/data_protection_menu_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}

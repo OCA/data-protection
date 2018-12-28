a39c5743692ea11ece784db33710389e72033198
aheficent <ahenriquez@eficent.com>
2018-12-28 15:55:35 +0100

    [FIX]Do not pass the data to the report. Do not encode values.

--- privacy_partner_report/readme/ROADMAP.rst
+++
@@ -1,1 +0,0 @@
-* Remove controller workaround if https://github.com/odoo/odoo/pull/24964 is merged.
--- privacy_partner_report/report/privacy_partner_xlsx.py
+++ privacy_partner_report/report/privacy_partner_xlsx.py
@@ -22,58 +22,62 @@ class ReportPartnerXlsx(models.AbstractModel):
         return res
 
     def generate_xlsx_report(self, workbook, data, objects):
-        partner = data['form'].get('partner_id', False)
-        partner = self.env['res.partner'].sudo().browse(partner[0])
-        workbook.set_properties({
-            'comments': 'Created with Python and XlsxWriter from Odoo'})
-        sheet = workbook.add_worksheet(_('Partner Data'))
-        sheet.set_landscape()
-        sheet.fit_to_pages(1, 0)
-        sheet.set_zoom(75)
-        sheet.set_column(0, self._search_longest_row(data['tables']), 25)
-        title_style = workbook.add_format(
-            {'bold': True, 'bg_color': '#FFFFCC', 'border': 2})
-        sheet.set_row(0, None, None, {'collapsed': 1})
-        sheet.write_row(1, 0, ["Partner: " + partner.display_name],
-                        title_style)
-        i = 3
-        first_row = i+2
-        for table in sorted(data['tables'].keys()):
-            for model in sorted(data['tables'][table].keys()):
-                rows = len(data['tables'][table][model])
-                if rows:
-                    style = workbook.add_format()
-                    style.set_bold(True)
-                    style.set_border(2)
-                    sheet.write_row(i, 0, [model], style)
-                    i += 1
-                    j = 0
-                    for column in data['tables'][table][model][0]:
+        for o in objects:
+            report_data = o.compute_data_for_report(data)
+            partner = report_data['form'].get('partner_id', False)
+            partner = self.env['res.partner'].sudo().browse(partner[0])
+            workbook.set_properties({
+                'comments': 'Created with Python and XlsxWriter from Odoo'})
+            sheet = workbook.add_worksheet(_('Partner Data'))
+            sheet.set_landscape()
+            sheet.fit_to_pages(1, 0)
+            sheet.set_zoom(75)
+            sheet.set_column(0, self._search_longest_row(
+                report_data['tables']), 25)
+            title_style = workbook.add_format(
+                {'bold': True, 'bg_color': '#FFFFCC', 'border': 2})
+            sheet.set_row(0, None, None, {'collapsed': 1})
+            sheet.write_row(1, 0, ["Partner: " + partner.display_name],
+                            title_style)
+            i = 3
+            first_row = i+2
+            for table in sorted(report_data['tables'].keys()):
+                for model in sorted(report_data['tables'][table].keys()):
+                    rows = len(report_data['tables'][table][model])
+                    if rows:
                         style = workbook.add_format()
                         style.set_bold(True)
-                        if j == 0:
-                            style.set_left(1)
-                        if j == len(data['tables'][table][model][0]) - 1:
-                            style.set_right(1)
-                        style.set_top(1)
-                        style.set_bottom(1)
-                        sheet.write_row(i, j, [column], style)
-                        j += 1
-                    for row in data['tables'][table][model]:
+                        style.set_border(2)
+                        sheet.write_row(i, 0, [model], style)
                         i += 1
                         j = 0
-                        for column in row:
+                        for column in report_data['tables'][table][model][0]:
                             style = workbook.add_format()
+                            style.set_bold(True)
                             if j == 0:
                                 style.set_left(1)
-                            if j == len(row) - 1:
+                            if j == len(report_data['tables'][
+                                        table][model][0]) - 1:
                                 style.set_right(1)
-                            if i == rows + first_row - 1:
-                                style.set_bottom(1)
-                            if row[column]:
-                                sheet.write_row(i, j, [row[column]], style)
-                            else:
-                                sheet.write_row(i, j, [''], style)
+                            style.set_top(1)
+                            style.set_bottom(1)
+                            sheet.write_row(i, j, [column], style)
                             j += 1
-                i += 2
-                first_row = i+2
+                        for row in report_data['tables'][table][model]:
+                            i += 1
+                            j = 0
+                            for column in row:
+                                style = workbook.add_format()
+                                if j == 0:
+                                    style.set_left(1)
+                                if j == len(row) - 1:
+                                    style.set_right(1)
+                                if i == rows + first_row - 1:
+                                    style.set_bottom(1)
+                                if row[column]:
+                                    sheet.write_row(i, j, [row[column]], style)
+                                else:
+                                    sheet.write_row(i, j, [''], style)
+                                j += 1
+                    i += 2
+                    first_row = i+2

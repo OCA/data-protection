0f6487f338618754517eea3df085677c90dd8068
Jairo Llopis <jairo.llopis@tecnativa.com>
2021-01-12 13:56:04 +0000

    [IMP] pre-commit run

--- privacy_consent/controllers/main.py
+++ privacy_consent/controllers/main.py
@@ -11,9 +11,12 @@ from odoo.addons.web.controllers.main import ensure_db
 
 
 class ConsentController(Controller):
-    @route("/privacy/consent/<any(accept,reject):choice>/"
-           "<int:consent_id>/<token>",
-           type="http", auth="none", website=True)
+    @route(
+        "/privacy/consent/<any(accept,reject):choice>/" "<int:consent_id>/<token>",
+        type="http",
+        auth="none",
+        website=True,
+    )
     def consent(self, choice, consent_id, token, *args, **kwargs):
         """Process user's consent acceptance or rejection."""
         ensure_db()
@@ -23,23 +26,26 @@ class ConsentController(Controller):
         except AttributeError:
             # If there's no website, the default is OK
             pass
-        consent = request.env["privacy.consent"] \
-            .with_context(subject_answering=True) \
-            .sudo().browse(consent_id)
+        consent = (
+            request.env["privacy.consent"]
+            .with_context(subject_answering=True)
+            .sudo()
+            .browse(consent_id)
+        )
         if not (consent.exists() and consent._token() == token):
             raise NotFound
         if consent.partner_id.lang:
             consent = consent.with_context(lang=consent.partner_id.lang)
             request.context = consent.env.context
         consent.action_answer(choice == "accept", self._metadata())
-        return request.render("privacy_consent.form", {
-            "consent": consent,
-        })
+        return request.render("privacy_consent.form", {"consent": consent})
 
     def _metadata(self):
-        return (u"User agent: {}\n"
-                u"Remote IP: {}\n"
-                u"Date and time: {:%Y-%m-%d %H:%M:%S}").format(
+        return (
+            u"User agent: {}\n"
+            u"Remote IP: {}\n"
+            u"Date and time: {:%Y-%m-%d %H:%M:%S}"
+        ).format(
             request.httprequest.environ.get("HTTP_USER_AGENT"),
             request.httprequest.environ.get("REMOTE_ADDRESS"),
             datetime.now(),

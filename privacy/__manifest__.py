f31ab2db9a36e41453f46bafdd321aa6b61ed5b6
Baty7 <juanjo.bautista@aulora.io>
2025-06-04 16:01:25 +0200

    [IMP] privacy: pre-commit auto fixes

--- privacy/__manifest__.py
+++ privacy/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Data Privacy and Protection",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Data Protection",
     "summary": "Provides data privacy and protection features "
